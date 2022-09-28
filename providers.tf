f38e45f9744bea45d01d0ef68e1541eca529b953
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-09-28 10:16:04 +0000

    Chore(template): Accept template changes
    - Add default providers
    - Add new default variables

--- providers.tf
+++ providers.tf
@@ -4,37 +4,6 @@ provider "aws" {
   profile = var.aws_profile
 
   default_tags {
-    tags = {
-      iac_environment = var.iac_environment_tag
-      ProjectName     = var.cluster_name
-      MetaRepo        = "nextjs-grpc"
-      Repo            = "infra/aws"
-      Region          = "DNS region"
-      DefaultProvider = "false"
-    }
-  }
-}
-
-provider "aws" {
-  region  = var.cluster_region
-  profile = var.aws_profile
-
-  default_tags {
-    tags = {
-      iac_environment = var.iac_environment_tag
-      ProjectName     = var.cluster_name
-      MetaRepo        = "nextjs-grpc"
-      Repo            = "infra/aws"
-      Region          = "Cluster region"
-      DefaultProvider = "true"
-    }
-  }
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.aws.cluster_endpoint
-    cluster_ca_certificate = module.aws.cluster_ca_certificate
-    token                  = module.aws.cluster_token
+    tags = var.aws_provider_default_tags
   }
 }
--- terraform.template.tf
+++ terraform.template.tf
@@ -1,8 +1,6 @@
 terraform {
   required_version = ">= 1.1.9"
 
-  # backend "s3" {}
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
