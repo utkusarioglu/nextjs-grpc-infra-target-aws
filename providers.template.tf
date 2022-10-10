55fa1be47c56b1ec320d59d132a63eb2f241164e
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-10-10 14:05:12 +0000

    Chore(providers): Add kubernetes provider
    - Add definition for kubernetes provider. This has to be done because
      `infra/configs/app` now directly uses kubernetes provider for
      certain resources, instead of always relying on helm.

--- providers.template.tf
+++ providers.template.tf
@@ -14,3 +14,9 @@ provider "helm" {
     token                  = module.aws.cluster_token
   }
 }
+
+provider "kubernetes" {
+  host                   = module.aws.cluster_endpoint
+  cluster_ca_certificate = module.aws.cluster_ca_certificate
+  token                  = module.aws.cluster_token
+}
